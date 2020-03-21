831b8a066d16f2c79844074a43583e16c5c0abe8
SALajes <sofialajes98@gmail.com>
2020-03-21 16:19:39 +0000

    Changed main

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,24 @@
 // (powered by Fernflower decompiler)
 //
 
+import java.io.FileNotFoundException;
+
 public class Main {
     public Main() {
     }
 
     public static void main(String[] args) {
+        try{
+            Javamm javamm = new Javamm(new java.io.FileInputStream(args[0]));
+            javamm.Start();
+        } catch(FileNotFoundException e){
+            System.out.println("File not found");
+            System.exit(-1);
+        }catch(ParseException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+
     }
 
     public static int eval(SimpleNode node) {
